edbbb3d2e40ba5c396b2a63c0688b961c5039def
Kikokushijo <zoo30215@gmail.com>
2017-10-06 17:30:32 +0800

    Can Serve Sentences as Batch

--- hw1/read_data.py
+++ hw1/read_data.py
@@ -33,22 +33,21 @@ class Dataset(object):
                 self.label_dict[name] = self.char_dict[label][0]
 
     def set_train_label(self):
-        while True:
-            with open(self.train_set_path, 'r') as f:
-                for line in f:
-                    name, *features = line.strip('\n').split(' ')
-                    spkrid, sentid, timeid = name.split('_')
-                    print(name)
+        with open(self.train_set_path, 'r') as f:
+            for line in f:
+                name, *features = line.strip('\n').split(' ')
+                spkrid, sentid, timeid = name.split('_')
 
-                    if  (spkrid != self.sentence.spkrid or
-                        sentid != self.sentence.sentid):
-                        if self.sentence.spkrid is not None:
-                            X_train = np.array(self.sentence.timestep_features).astype(np.float)
-                            X_train = np.lib.pad(X_train, ((0, 800-len(X_train)), (0, 0)), 'constant', constant_values=0)
-                            Y_train = np.array(self.sentence.timestep_label).astype(int)
-                            yield (X_train, Y_train)
-                        self.sentence = Sentence(spkrid, sentid)
-                    self.sentence.append_timestep(features, self.label_dict[name])
+                if  (spkrid != self.sentence.spkrid or
+                    sentid != self.sentence.sentid):
+                    if self.sentence.spkrid is not None:
+                        X_train = np.array(self.sentence.timestep_features).astype(np.float)
+                        X_train = np.lib.pad(X_train, ((0, 800-len(X_train)), (0, 0)), 'constant', constant_values=0)
+                        Y_train = np.array(self.sentence.timestep_label).astype(int)
+                        Y_train = np.lib.pad(Y_train, (0, 800-len(Y_train)), 'constant', constant_values=(0))
+                        yield (X_train, Y_train)
+                    self.sentence = Sentence(spkrid, sentid)
+                self.sentence.append_timestep(features, self.label_dict[name])                   
 
     def set_reduce_dict(self):
         with open(self.reduce_dict_path) as f:
@@ -56,25 +55,41 @@ class Dataset(object):
                 original, convert = line.strip('\n').split('\t')
                 self.reduce_dict[original] = convert
 
+
 class Sentence(object):
     def __init__(self, spkrid=None, sentid=None):
         self.spkrid = spkrid
         self.sentid = sentid
-        self.timestep_features = np.array([])
-        self.timestep_label = np.array([])
+        self.timestep_features = []
+        self.timestep_label = []
 
     def append_timestep(self, feature, label):
-        self.timestep_features = np.append(self.timestep_features, feature)
-        self.timestep_label = np.append(self.timestep_label, label)
+        self.timestep_features.append(feature)
+        self.timestep_label.append(label)
 
-class Sentence_Batch(object):
-    def __init__(self):
-        pass
+class SentenceBatch(object):
+    def __init__(self, sent_gen, batch_size=32):
+        self.x_train = []
+        self.y_train = []
+        self.batch_size = batch_size
+        self.sent_gen = sent_gen
+
+    def collect_sentence(self):
+        while True:
+            while len(self.x_train) < self.batch_size:
+                X, Y = next(self.sent_gen)
+                self.x_train.append([X])
+                self.y_train.append([Y])
+            yield (np.vstack(self.x_train),
+                   np.vstack(self.y_train))
+            self.x_train = []
+            self.y_train = []
 
 if __name__ == "__main__":
     DS = Dataset()
-    data_gen = DS.set_train_label()
+    SB = SentenceBatch(DS.set_train_label(), batch_size=2)
+    batch_gen = SB.collect_sentence()
     for i in range(3):
-        X, Y = next(data_gen)
-
-
+        X, Y = next(batch_gen)
+        print('X.shape', X.shape)
+        print('Y.shape', Y.shape)
