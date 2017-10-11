89bc5fcb5d3411ce7ceb7290ef51f7ab4930994b
Kikokushijo <zoo30215@gmail.com>
2017-10-12 03:45:43 +0800

    Have a model

--- hw1/model_rnn.py
+++ hw1/model_rnn.py
@@ -1,21 +1,26 @@
-from read_data import Dataset, SentenceBatch
+from read_data import Dataset
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.recurrent import LSTM
+from keras.layers.embeddings import Embedding
 
 if __name__ == "__main__":
-	DS = Dataset()
-    SB = SentenceBatch(DS.set_train_label(), batch_size=32)
-    batch_gen = SB.collect_sentence()
+    DS = Dataset(r'C:/Users/User/Desktop/dataset')
+    X, Y = DS.set_train_label()
 
-    model = Sequential
-    model.add(LSTM(32, input_shape=(800, 69)))
-    model.add(Dropout(0.5))
-    model.add(LSTM(16))
-    model.add(Dense(units=256, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(units=1, activation='softmax'))
+    model = Sequential()
+    model.add(Masking(mask_value=0.0, input_shape=(800, 69)))
+    model.add(LSTM(100, return_sequences=True))
+    model.add(Dropout(0.4))
+    model.add(TimeDistributed(Dense(48, activation='softmax')))
+    model.compile(loss='categorical_crossentropy', optimizer='adagrad', metrics=['accuracy'])
     print(model.summary())
 
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	
+    for _ in range(10):
+        try:
+            model.load_weights('D:/Data/ADLxMLDS2017/hw1/models/TimitRnnModel.h5')
+        except:
+            pass
+
+        train_history = model.fit(X, Y, batch_size=100, epochs=10, verbose=1, validation_split=0.2)
+        model.save_weights('D:/Data/ADLxMLDS2017/hw1/models/TimitRnnModel.h5')
--- hw1/read_data.py
+++ hw1/read_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 class Dataset(object):
     def __init__(self, path='../hw1_dataset/'):
@@ -6,24 +7,23 @@ class Dataset(object):
         self.char_dict = {}
         self.label_dict = {}
         self.reduce_dict = {}
-        self.sentence = Sentence()
         
-        self.char_dict_path = path + '48phone_char.map'
+        self.char_dict_path = os.path.join(path, '48phone_char.map')
         self.set_char_dict()
 
-        self.label_dict_path = path + 'label/train.lab'
+        self.label_dict_path = os.path.join(path, 'label', 'train.lab')
         self.set_label_dict()
 
-        self.reduce_dict_path = path + 'phones/48_39.map'
+        self.reduce_dict_path = os.path.join(path, 'phones', '48_39.map')
         self.set_reduce_dict()
 
-        self.train_set_path = path + 'fbank/train.ark'
+        self.train_set_path = os.path.join(path, 'fbank', 'train.ark')
 
     def set_char_dict(self):
         with open(self.char_dict_path) as f:
             for line in f.readlines():
                 phone, num, char = line.strip('\n').split('\t')
-                self.char_dict[phone] = [num, char]
+                self.char_dict[phone] = [int(num), char]
 
     def set_label_dict(self):
         with open(self.label_dict_path, 'r') as f:
@@ -33,20 +33,23 @@ class Dataset(object):
 
     def set_train_label(self):
         with open(self.train_set_path, 'r') as f:
+            sentence_dict = {}
             for line in f:
                 name, *features = line.strip('\n').split(' ')
-                spkrid, sentid, timeid = name.split('_')
-
-                if  (spkrid != self.sentence.spkrid or
-                    sentid != self.sentence.sentid):
-                    if self.sentence.spkrid is not None:
-                        X_train = np.array(self.sentence.timestep_features).astype(np.float)
-                        X_train = np.lib.pad(X_train, ((0, 800-len(X_train)), (0, 0)), 'constant', constant_values=0)
-                        Y_train = np.array(self.sentence.timestep_label).astype(int)
-                        Y_train = np.lib.pad(Y_train, (0, 800-len(Y_train)), 'constant', constant_values=(0))
-                        yield (X_train, Y_train)
-                    self.sentence = Sentence(spkrid, sentid)
-                self.sentence.append_timestep(features, self.label_dict[name])                   
+                spkird, sentid, timeid = name.split('_')
+                key = "%s_%s" % (spkird, sentid)
+                if key not in sentence_dict:
+                    sentence_dict[key] = Sentence(key)
+                sentence_dict[key].feature[int(timeid)-1][:] = np.array(features).astype(np.float)
+                sentence_dict[key].label[int(timeid)-1][self.label_dict[name]] = 1.0
+        
+        all_feature = []
+        all_label = []
+        for pair in sentence_dict.items():
+            all_feature.append([pair[1].feature])
+            all_label.append([pair[1].label])
+        
+        return (np.vstack(all_feature), np.vstack(all_label))
 
     def set_reduce_dict(self):
         with open(self.reduce_dict_path) as f:
@@ -54,32 +57,10 @@ class Dataset(object):
                 original, convert = line.strip('\n').split('\t')
                 self.reduce_dict[original] = convert
 
-
 class Sentence(object):
-    def __init__(self, spkrid=None, sentid=None):
-        self.spkrid = spkrid
-        self.sentid = sentid
-        self.timestep_features = []
-        self.timestep_label = []
-
-    def append_timestep(self, feature, label):
-        self.timestep_features.append(feature)
-        self.timestep_label.append(label)
-
-class SentenceBatch(object):
-    def __init__(self, sent_gen, batch_size=32):
-        self.x_train = []
-        self.y_train = []
-        self.batch_size = batch_size
-        self.sent_gen = sent_gen
-
-    def collect_sentence(self):
-        while True:
-            while len(self.x_train) < self.batch_size:
-                X, Y = next(self.sent_gen)
-                self.x_train.append([X])
-                self.y_train.append([Y])
-            yield (np.vstack(self.x_train),
-                   np.vstack(self.y_train))
-            self.x_train = []
-            self.y_train = []
+    def __init__(self, name, feature_width=69, max_length=800, char_variety=48):
+        self.name = name
+        self.feature_width = feature_width
+        self.max_length = max_length
+        self.feature = np.zeros((max_length, feature_width))
+        self.label = np.zeros((max_length, char_variety))
