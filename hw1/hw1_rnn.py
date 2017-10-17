b39e7125e85067d1bb9694def2adb8f57331ddfe
Kikokushijo <zoo30215@gmail.com>
2017-10-18 00:21:47 +0800

    add new filter

--- hw1/hw1_rnn.py
+++ hw1/hw1_rnn.py
@@ -2,8 +2,8 @@ from read_data import Dataset
 from model_rnn import Model
 import os
 
-def output_result(filter, output_filename, predict):
-    if filter == 'from_class':
+def output_result(filters, output_filename, predict):
+    if filters == 'from_class':
         with open(output_filename, 'w') as f:
             f.write('id,phone_sequence\n')
             for index, row in enumerate(predict):
@@ -24,7 +24,7 @@ def output_result(filter, output_filename, predict):
                         result += DS.num2char[record[i]]
                 f.write('%s,%s\n' % (key[index], result[1:]))
                 print(result)
-    if filter == 'from_prob':
+    if filters == 'from_prob':
         with open('mfcc_fbank.csv', 'w') as f:
             f.write('id,phone_sequence\n')
             for index, sentence in enumerate(mfcc_fbank):
@@ -50,6 +50,49 @@ def output_result(filter, output_filename, predict):
                         result += DS.num2char[record[i]]
                 f.write('%s,%s\n' % (key[index], result[1:]))
                 print(result)
+
+    if filters == 'repeat':
+        rows = []
+        with open('output/CRC_mfcc_97.csv', 'w') as f:
+            f.write('id,phone_sequence\n')
+            for index, sentence in enumerate(predict_p):
+                
+                length = name[index][1]
+                padding_times = 800 // length
+                prob_sequence = []
+                for v_index, v_i in enumerate(sentence[:length]):
+                    for _ in range(1, padding_times):
+                        v_i += sentence[v_index + _ * length]
+                    prob_sequence.append(v_i)
+                
+                assert len(prob_sequence) == length
+                
+                row = []
+                for vector in prob_sequence:
+                    classes, prob = max(enumerate(vector), key=lambda x:x[1])
+                    row.append(classes)
+                rows.append(row)
+                
+                assert len(row) == length
+                
+                frame_width = 7
+                start = 0
+                record = []
+                while start + frame_width < len(row):
+                    block = list(row[start:start+frame_width])
+                    max_occurred = max(block, key=block.count)
+                    if block.count(max_occurred) >= 3:
+                        record.append(max_occurred)
+                    start += 1
+
+                print('Index: ', index, 'result: ', record)
+                result = ""
+                for i in range(len(record)-1):
+                    if record[i] != record[i+1]:
+                        result += DS_mfcc.num2char[record[i]]
+                f.write('%s,%s\n' % (name[index][0], result[1:]))
+                print(result)
+
 
 if __name__ == "__main__":
 
