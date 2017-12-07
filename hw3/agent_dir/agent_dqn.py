52b440b5aa027b3dd1c585f8ad0aeabd2ff19e79
kikokushijo <zoo30215@gmail.com>
2017-12-08 05:34:30 +0800

    Can Train!

--- hw3/agent_dir/agent_dqn.py
+++ hw3/agent_dir/agent_dqn.py
@@ -1,19 +1,4 @@
 from agent_dir.agent import Agent
-from model.torch_models import DQNCNN, SimpleDQN
-from util.schedule import LinearSchedule
-from util.variable import Variable
-from util.buffer import Buffer
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.autograd as autograd
-
-import numpy as np
-import random
-import os
-
-dtype = torch.cuda.DoubleTensor
 
 class Agent_DQN(Agent):
     def __init__(self, env, args):
@@ -23,47 +8,15 @@ class Agent_DQN(Agent):
         """
 
         super(Agent_DQN,self).__init__(env)
-        self.num_actions = env.action_space.n
-        self.schedule = LinearSchedule()
-        self.Q_filename = os.path.join('model', 'DQN_Q.pkl')
-        self.targetQ_filename = os.path.join('model', 'DQN_targetQ.pkl')
-        self.build_model()
-        self.buffer_size = int(8e4)
-        self.buffer = Buffer(self.buffer_size)
-        self.gamma = 0.99
-        self.training_record = os.path.join('record', 'DQN.csv')
-
-        self.loss_func = nn.MSELoss()
 
         if args.test_dqn:
             #you can load your model here
             print('loading trained model')
-        else:
-            pass
-        
 
-    def build_model(self):
-        # self.Q = DQNCNN(self.num_actions).type(dtype)
-        # self.target_Q = DQNCNN(self.num_actions).type(dtype)
-        self.Q = SimpleDQN(self.num_actions).type(dtype)
-        self.target_Q = SimpleDQN(self.num_actions).type(dtype)
-        if os.path.isfile(self.Q_filename):
-            print('Load Q parametets ...')
-            self.Q.load_state_dict(torch.load(self.Q_filename))
-        else:
-            print('Start Training From Scratch')
-        
-        if os.path.isfile(self.targetQ_filename):
-            print('Load target Q parameters ...')
-            self.target_Q.load_state_dict(torch.load(self.targetQ_filename))
-        else:
-            print('Start Training From Scratch')
+        ##################
+        # YOUR CODE HERE #
+        ##################
 
-        self.opt = optim.RMSprop(self.Q.parameters(), lr=0.00001, alpha=0.95, eps=0.01)
-
-    def save_model(self):
-        torch.save(self.Q.state_dict(), self.Q_filename)
-        torch.save(self.target_Q.state_dict(), self.targetQ_filename)
 
     def init_game_setting(self):
         """
@@ -72,20 +25,11 @@ class Agent_DQN(Agent):
         Put anything you want to initialize if necessary
 
         """
+        ##################
+        # YOUR CODE HERE #
+        ##################
+        pass
 
-        self.state = self.env.reset()
-
-    def select_training_action(self, state, t):
-        rd = random.random()
-        eps = self.schedule.value(t)
-        if rd > eps:
-            # print(state.shape)
-            state = torch.from_numpy(np.transpose(state, (2, 0, 1))).type(dtype).unsqueeze(0) / 255.0
-            # print(state.shape)
-            # print(self.Q(Variable(state, volatile=True)).data.max(1)[1].cpu())
-            return self.Q(Variable(state, volatile=True)).data.max(1)[1].cpu()
-        else:
-            return torch.IntTensor([[random.randrange(self.num_actions)]])
 
     def train(self):
         """
@@ -94,81 +38,7 @@ class Agent_DQN(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
-
-        from itertools import count
-
-        self.init_game_setting()
-
-        num_updates = 0
-        target_update_freq = 10000
-        num_episode = 0
-        episode_reward = 0
-        episode_rewards = []
-
-        for t in count():
-
-            if t % 100 == 0:
-                print('Step %d' %(t))
-
-            if t > self.buffer_size:
-                # action = self.select_training_action(self.state, t)[0, 0]
-                action = int(self.select_training_action(self.state, t)[0])
-            else:
-                action = int(random.randrange(self.num_actions))
-
-            # print('Action:', action)
-
-            next_state, reward, done, _ = self.env.step(action)
-            episode_reward += reward
-            reward = max(-1.0, min(reward, 1.0))
-            self.buffer.append(self.state, action, reward, next_state, done)
-            self.state = next_state
-
-            if done:
-                self.init_game_setting()
-                num_episode += 1
-                episode_rewards.append(episode_reward)
-                episode_reward = 0
-                if t > self.buffer_size:
-                    with open(self.training_record, 'a') as f:
-                        f.write('%d, %d\n' %(num_episode, episode_rewards[-1]))
-
-            if t > self.buffer_size:
-                s_batch, a_batch, r_batch, ns_batch, done_mask = self.buffer.sample()
-                
-                s_batch = Variable(torch.from_numpy(np.asarray(np.transpose(s_batch, (0, 3, 1, 2)))).type(dtype) / 255.0)
-                a_batch = Variable(torch.from_numpy(np.asarray(a_batch)).long()).cuda()
-                r_batch = Variable(torch.from_numpy(np.asarray(r_batch))).cuda()
-                ns_batch = Variable(torch.from_numpy(np.asarray(np.transpose(ns_batch, (0, 3, 1, 2)))).type(dtype) / 255.0)
-                nd_mask = Variable(torch.from_numpy(1 - np.asarray(done_mask))).type(dtype)
-                # print('Size', s_batch.size())
-
-                # To Be Viewed 
-                current_Q_values = self.Q(s_batch).gather(1, a_batch.unsqueeze(1))
-                next_max_q = self.target_Q(ns_batch).detach().max(1)[0]
-                next_Q_values = nd_mask * next_max_q
-                target_Q_values = r_batch + (self.gamma * next_Q_values)
-                loss = self.loss_func(current_Q_values, target_Q_values)
-                if t % 1000 == 0:
-                    print(loss)
-
-                self.opt.zero_grad()
-                loss.backward()
-
-                self.opt.step()
-                num_updates += 1
-
-                if num_updates % target_update_freq == 0:
-                    self.target_Q.load_state_dict(self.Q.state_dict())
-
-
-                
-            if t % 10000 == 0 and t > self.buffer_size:
-                self.save_model()
-            
-
-            # with open(self.training_record, 'a') as f:
-            #     f.write('%d, %d\n' %(num_episode, mean_episode_reward))
+        pass
 
 
     def make_action(self, observation, test=True):
@@ -186,11 +56,5 @@ class Agent_DQN(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
-
-        if test:
-            pass
-        else:
-            pass
-
         return self.env.get_random_action()
 
--- hw3/agent_dir/agent_pg.py
+++ hw3/agent_dir/agent_pg.py
@@ -2,6 +2,8 @@ from agent_dir.agent import Agent
 import scipy
 import numpy as np
 
+import torch.optim as optim
+
 def prepro(o,image_size=[80,80]):
     """
     Call this function to preprocess RGB image to grayscale image if necessary
@@ -28,7 +30,13 @@ class Agent_PG(Agent):
         For example: building your model
         """
 
-        super(Agent_PG,self).__init__(env)
+        super(Agent_PG, self).__init__(env)
+        self.policy_network = PolicyNetwork()
+        if os.path.isfile('pg_params.pkl'):
+            print('Load Policy Network parametets ...')
+            self.policy_network.load_state_dict(torch.load('pg_params.pkl'))
+        self.opt = optim.RMSprop(self.policy_network.parameters(), lr=1e-4, weight_decay=0.99)
+        self.gamma = 0.99
 
         if args.test_pg:
             #you can load your model here
@@ -51,6 +59,28 @@ class Agent_PG(Agent):
         ##################
         pass
 
+    def finish_episode():
+        R = 0
+        policy_loss = []
+        rewards = []
+        for r in policy.rewards[::-1]:
+            R = r + self.gamma * R
+            rewards.insert(0, R)
+        # turn rewards to pytorch tensor and standardize
+        rewards = torch.Tensor(rewards)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
+        
+        for log_prob, reward in zip(self.policy_network.saved_log_probs, rewards):
+            policy_loss.append(-log_prob * reward)
+
+        self.opt.zero_grad()
+        policy_loss = torch.cat(policy_loss).sum()
+        policy_loss.backward()
+        self.opt.step()
+
+        # clean rewards and saved_actions
+        del policy.rewards[:]
+        del policy.saved_log_probs[:]
 
     def train(self):
         """
@@ -59,7 +89,36 @@ class Agent_PG(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
-        pass
+        running_reward = None
+        reward_sum = 0
+        for i_episode in count(1):
+            state = self.env.reset()
+            for t in range(10000):
+                action = make_action(state)
+                action = action + 1
+                state, reward, done, _ = env.step(action)
+                reward_sum += reward
+                
+                self.policy_network.rewards.append(reward)
+                if done:
+                    # tracking log
+                    running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
+                    print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
+                    reward_sum = 0
+                    break
+                    
+                if reward != 0:
+                    print('ep %d: game finished, reward: %f' % (i_episode, reward) + ('' if reward == -1 else ' !!!!!!!'))
+
+            # use policy gradient update model weights
+            if i_episode % args.batch_size == 0:
+                print('ep %d: policy network parameters updating...' % (i_episode))
+                self.finish_episode()
+
+            # Save model in every 50 episode
+            if i_episode % 50 == 0:
+                print('ep %d: model saving...' % (i_episode))
+                torch.save(policy.state_dict(), 'pg_params.pkl')
 
 
     def make_action(self, observation, test=True):
@@ -77,5 +136,42 @@ class Agent_PG(Agent):
         ##################
         # YOUR CODE HERE #
         ##################
-        return self.env.get_random_action()
+        state = prepro(state)
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        probs = policy(Variable(state))
+        m = Categorical(probs)
+        action = m.sample()
+        policy.saved_log_probs.append(m.log_prob(action))
+        return action.data[0]
 
+class PolicyNetwork(nn.Module):
+    def __init__(self):
+        super(PolicyNetwork, self).__init__()
+
+        # self.conv1 = nn.Sequential(
+        #     nn.Conv2d(1, 16, 5, 1, 2),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(2),
+        # )
+
+        # self.conv2 = nn.Sequential(
+        #     nn.Conv2d(16, 32, 5, 1, 2),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(2),
+        # )
+
+        # self.affine1 = nn.Linear(32 * 20 * 20, 3)
+
+        self.affine1 = nn.Linear(6400, 64)
+        self.affine2 = nn.Linear(64, 3)
+        self.saved_log_probs = []
+        self.rewards = []
+        
+    def forward(self, x):
+        x = F.relu(self.affine1(x))
+        action_scores = self.affine2(x)
+        # x = self.conv1(x.view(-1, 1, 80, 80))
+        # x = self.conv2(x)
+        # x = x.view(x.size(0), -1)
+        # action_scores = self.affine1(x)
+        return F.softmax(action_scores, dim=1)
